12181aa3144a07a0bc1b2ba1a827bdb6aed02637
Bogdan Lytvyn <boddofish@gmail.com>
2021-02-08 11:17:52 +0200

    Доопрацював лайтбокс

--- src/js/ligthbox.js
+++ src/js/ligthbox.js
@@ -2,23 +2,18 @@ import refs from "./refs.js";
 const basicLightbox = require("basiclightbox");
 
 function openLightboxHandler(event) {
-  let currentImgURL = event.target.dataset.source;
+  const currentImgURL = event.target.dataset.source;
   basicLightbox
     .create(
-      `<div class ="lightbox"><img class ="img-lightbox" src="${currentImgURL}"></div>`,
-      {
-        closable: true,
-      }
+      `<div class ="lightbox"> <img class ="img-lightbox" src="${currentImgURL}"> </div>`
     )
     .show();
-  let lightbox = document.querySelector(".basicLightbox");
-  closeLightbox(lightbox);
-}
 
-function closeLightbox(lightbox) {
+  const lightbox = document.querySelector(".basicLightbox");
   function closeLightboxHandler() {
     lightbox.remove();
   }
   lightbox.addEventListener("click", closeLightboxHandler);
 }
+
 refs.gallery.addEventListener("click", openLightboxHandler);
